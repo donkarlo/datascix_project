ee1ea98dfbc49cae540d382e0dad9b816e8d4a9d
Mohammad Rahmani <mohammad.rahmani.xyz@gmail.com>
2025-12-27 21:40:04 +0100

    seq2seq

--- src/datascix/ml/seq2seq/transformer/seq_to_seq.py
+++
@@ -1,5 +0,0 @@
-from datascix.ml.seq2seq.transformer.transformer import Transformer
-
-
-class SeqToSeq(Transformer):
-    pass
---
+++ src/datascix/ml/supervision/self_supervised.py
@@ -0,0 +1,5 @@
+from typing import Protocol
+
+
+class SelfSupervised(Protocol):
+    pass
